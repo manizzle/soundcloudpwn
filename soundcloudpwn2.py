8fe3811304caecc26026e0d56cdd4e42c76a43c5
manizzle <devnull@localhost>
2013-05-28 00:56:24 -0700

    round1 of gui dev

--- soundcloudpwn2.py
+++ soundcloudpwn2.py
@@ -5,11 +5,13 @@
 
 import os, urllib2, sys, urllib, ujson, requests, lxml.html, random, math
 from Tkinter import *
+import tkSimpleDialog
 someones_client_id = "b45b1aa10f1ac2941910a7f0d10f8e28"
 #someones_client_id = "7dd86f1df1b1f7f08683ffc8b5a39b23"
 SHAME_LIMIT = 10
 progress_bar_size = 64
 
+obj = None
 
 class ScrolledText(Text):
     def __init__(self, frame, **kw):
@@ -35,39 +37,43 @@ class ScrolledText(Text):
         return str(self.frame)
 
 class App:
-
     def __init__(self, master):
-
+        global obj
         self.master = master
-
         frame = Frame(master)
         frame.pack()
-
         self.text = ScrolledText(frame, bg='white', height=10, font="Courier")
         self.text.pack(fill=BOTH, side=LEFT, expand=True)
-        self.text.insert(END, "lollerstaces")
 
+        obj = (self.text, self.master)
 
         self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
         self.button.pack(side=RIGHT)
 
-        self.go = Button(frame, text="Go", command=self.go)
-        self.go.pack(side=LEFT)
+        self.goz = Button(frame, text="Go", command=lambda : self.go(self.master))
+        self.goz.pack(side=LEFT)
 
-        self.shame = Button(frame, text="Shame", command=self.shame)
-        self.shame.pack(side=LEFT)
+        self.shamez = Button(frame, text="Shame", command=self.shamez)
+        self.shamez.pack(side=LEFT)
 
-    def shame(self, all_the_things=False):
+    def say_high(self):
+        print "dwfsd"
+
+    def shamez(self, all_the_things=False):
         shame(all_the_things)
 
-    def go(self, artist):
-        artist = tkSimpleDialog.askstring("SoundCloudPwn", prompt, "whouwant?", self.master)
+    def go(self, master):
+        artist = tkSimpleDialog.askstring("SoundCloudPwn", "Artist:", initialvalue="whouwant?", parent=master)
         dl_sc(artist)
-    
 
-
-
-
+def d(st):
+    global obj
+    if obj:
+        t, m = obj
+        t.insert(END, st)
+        m.update_idletasks()
+    else:
+        print >>sys.stderr, st
 
 def get_lucky_url(name, site=None):
     base  = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q='
@@ -86,20 +92,20 @@ def get_tracks(username):
     try:
         whouwant = whouwant.decode("utf-8")
     except  UnicodeEncodeError as e:
-        print >>sys.stderr, "[E] Username of %s is foobar" % (repr(whouwant))
+        d("[E] Username of %s is foobar" % (repr(whouwant)))
         return []
     yy = "http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/%s&client_id=%s" % (urllib.quote(whouwant), someones_client_id)
     zz = requests.head(yy)
     # the given username was not a valid account name
     if  zz.status_code == 404:
         # http, null, soundcloud, artist, ...
-        print >>sys.stderr, "[E] could not find artist %s, asking google for best match" % shorten(whouwant, 20)
+        d("[E] could not find artist %s, asking google for best match" % shorten(whouwant, 20))
         whouwant = get_lucky_url(whouwant, "soundcloud.com").split("/")[3].decode("utf-8")
-    print >>sys.stderr, "[+] User: %s ->" % whouwant
+    d("[+] User: %s ->\n" % whouwant)
     yy = "http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/%s/tracks&client_id=%s" % (urllib.quote(whouwant), someones_client_id)
     reqzz = requests.head(yy)
     if reqzz.status_code == 404:
-        print >>sys.stderr, "[E] Could not find track listing for user %s" % shorten(whouwant, 20)
+        d("[E] Could not find track listing for user %s" % shorten(whouwant, 20))
         return []
     zz = urllib2.urlopen(yy).read()
     zz_uj = ujson.loads(zz)
@@ -119,16 +125,16 @@ def shame(all_the_things=False):
         resp = requests.head(yy)
         if not resp.status_code == 404:
             zz = urllib2.urlopen(yy).read()
-            obj = lxml.html.document_fromstring(zz)
-            id_username = obj.xpath("//username")[0].text
-            id_account_type = obj.xpath("//plan")[0].text
-            print >>sys.stderr, failures
+            hobj = lxml.html.document_fromstring(zz)
+            id_username = hobj.xpath("//username")[0].text
+            id_account_type = hobj.xpath("//plan")[0].text
+            d(str(failures) + "\n")
             failures = 0
-            print >>sys.stderr, id_account_type
+            d(id_account_type + "\n")
             tracks = get_tracks(id_username)
             for c in tracks:
                 if not c['downloadable']:
-                    print >>sys.stderr, '\t'  + repr(c['title']) +  "    " + c['stream_url'] + "?client_id=%s" % (someones_client_id)
+                    d('\t'  + repr(c['title']) +  "    " + c['stream_url'] + "?client_id=%s" % (someones_client_id))
             if tracks:
                 ctr +=1
             if not all_the_things and ctr == SHAME_LIMIT:
@@ -146,10 +152,10 @@ def shame(all_the_things=False):
 def make_artist_dir(target):
     cleaned = clean(target)
     if not os.path.isdir(cleaned):
-        print >>sys.stderr, "[+] Making new directory " + cleaned
+        d("[+] Making new directory " + cleaned)
         os.mkdir(cleaned)
     else:
-        print >>sys.stderr, "[+] Using existing directory " + cleaned
+        d("[+] Using existing directory " + cleaned)
     return cleaned            
 
 def clean(url):
@@ -176,18 +182,18 @@ def dl_sc(username):
     tracks = get_tracks(username)
     numtracks = len(tracks)
     if(numtracks == 0):        
-        print >>sys.stderr, "[+] No tracks found"
+        d("[+] No tracks found")
         return []
     os.chdir(make_artist_dir(username))
     for i,c in enumerate(tracks):
-        print >>sys.stderr, "[+][%s/%s] %s | %s" % (str(i+1).rjust(3, '0'), str(numtracks).rjust(3, '0'), shorten(repr(c['title'])[2:-1], 38).ljust(38, ' ') ,  "thank you %s!" % shorten(username, 15) if c['downloadable'] else "cause %s sux!" % shorten(username, 15))
+        d("[+][%s/%s] %s | %s" % (str(i+1).rjust(3, '0'), str(numtracks).rjust(3, '0'), shorten(repr(c['title'])[2:-1], 38).ljust(38, ' ') ,  "thank you %s!" % shorten(username, 15) if c['downloadable'] else "cause %s sux!" % shorten(username, 15)))
         full_url = c['stream_url'] + "?client_id=%s" % (someones_client_id)
         zz = urllib2.urlopen(full_url)
         file_size = int(zz.info().getheaders("Content-Length")[0])
         f = open(c['title'].replace(" ", "_").replace("/", " ") + ".mp3", "w")
         file_size_dl = 0
         dl_block_sz = file_size / progress_bar_size
-        print >>sys.stderr, "[+] %s " % convertSize(file_size).ljust(9, ' '),
+        d("[+] %s\n " % convertSize(file_size).ljust(9, ' '))
         while True:
             buffer = zz.read(dl_block_sz);
             if not buffer:
@@ -195,14 +201,12 @@ def dl_sc(username):
             file_size_dl += len(buffer)
             f.write(buffer)
             # because newlines/spaces are dumb
-            sys.stdout.write('.')
-            sys.stdout.flush()
+            d('.')
             
-        print >>sys.stderr
+        d("\n")
         #f.write(zz.read())
         f.close()
     os.chdir("..")
-    
 
 if __name__ == "__main__":
     root = Tk()
